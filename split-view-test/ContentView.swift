cfc833d2a879108867f8373b01bbe602eee17851
Mike Engel <mike@mike-engel.com>
2021-05-25 13:08:24 +0200

    Initial Commit

---
+++ split-view-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  split-view-test
+//
+//  Created by Michael Engel on 25.05.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ split-view-test/split_view_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  split_view_testApp.swift
+//  split-view-test
+//
+//  Created by Michael Engel on 25.05.21.
+//
+
+import SwiftUI
+
+@main
+struct split_view_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
